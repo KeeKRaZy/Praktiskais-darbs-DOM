7528766c4a4494c6e361a2ac455bf247992b158a
KeeKRaZy <78360343+KeeKRaZy@users.noreply.github.com>
2021-03-23 17:02:08 +0200

    Add files via upload

---
+++ 06-detecting-typed-input/global.js
@@ -0,0 +1,13 @@
+window.addEventListener("load", function(){
+
+    var name = document.getElementById("full_name");
+  
+    name.addEventListener("keyup", function() {
+  
+      var greeting = document.getElementById("greeting");
+  
+      greeting.innerHTML = ("Hello there, " + name.value + "!");
+  
+    });
+  
+  });
